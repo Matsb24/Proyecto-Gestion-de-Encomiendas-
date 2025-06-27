c74636256fcd4b679479c829e3c02bf0a0b394de
Matb24 <matiasbch2408@gmail.com>
2025-06-27 13:42:30 -0500

    limpieza de codigo y mejora encriptacion
    Se eliminaron funciones que ya no se utilizan o se utilizaran en la sección de ordenar datos. Se mejoro el sistema de encriptación para que sea de una sola via

--- SistemaEncomiendas/src/utils/Encriptar/Encriptar.java
+++ SistemaEncomiendas/src/utils/Encriptar/Encriptar.java
@@ -1,38 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package utils.Encriptar;
 
-import java.util.Base64;
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
+import org.mindrot.jbcrypt.BCrypt;
 
 /**
  *
  * @author Matias
  */
 public class Encriptar {
-        private static final String ALGORITHM = "AES";
-    private static final String TRANSFORMATION = "AES";
-    private static final byte[] keyBytes = "1234567890123456".getBytes(); // Clave de 16 bytes
-
-    // Método para encriptar
-    public static String encrypt(String data) throws Exception {
-        SecretKeySpec key = new SecretKeySpec(keyBytes, ALGORITHM);
-        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encryptedData = cipher.doFinal(data.getBytes());
-        return Base64.getEncoder().encodeToString(encryptedData);
+    // Genera el hash de la contraseña
+    public static String hashPassword(String plainPassword) {
+        return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
     }
 
-    // Método para desencriptar
-    public static String decrypt(String encryptedData) throws Exception {
-        SecretKeySpec key = new SecretKeySpec(keyBytes, ALGORITHM);
-        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-        cipher.init(Cipher.DECRYPT_MODE, key);
-        byte[] decodedData = Base64.getDecoder().decode(encryptedData);
-        byte[] decryptedData = cipher.doFinal(decodedData);
-        return new String(decryptedData);
+    // Verifica si la contraseña ingresada coincide con el hash guardado
+    public static boolean checkPassword(String plainPassword, String hashedPassword) {
+        return BCrypt.checkpw(plainPassword, hashedPassword);
     }
 }
